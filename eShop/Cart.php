bc31f1f101c62db17c11fb0870389c95d111f3b4
vikasreddy0011 <54327238+vikasreddy0011@users.noreply.github.com>
2020-09-18 21:24:35 +0530

    Updated cart.php by removing comment

--- eShop/Cart.php
+++ eShop/Cart.php
@@ -1,12 +1,13 @@
 <?php session_start();
-class Cart {
+class Cart{
     protected $cart_contents = array();
     
     public function __construct(){
         // get the shopping cart array from the session
         $this->cart_contents = !empty($_SESSION['cart_contents'])?$_SESSION['cart_contents']:NULL;
-		if ($this->cart_contents === NULL){
-			// set some base values
+		if ($this->cart_contents === NULL)
+			
+		{
 			$this->cart_contents = array('cart_total' => 0, 'total_items' => 0);
 		}
     }
